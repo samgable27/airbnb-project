382804fbd26a665aa513fe495729db6f2984bcac
Sam <samarooniii@yahoo.com>
2023-09-14 17:01:43 -0400

    application finished

---
+++ app/reservations/ReservationsClient.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { SafeReservation, SafeUser } from "../types";
+import Heading from "../components/Heading";
+import Container from "../components/Container";
+import ListingCard from "../components/listings/ListingCard";
+import { useRouter } from "next/navigation";
+
+interface ReservationsClientProps {
+  reservations: SafeReservation[];
+  currentUser?: SafeUser | null;
+}
+
+const ReservationsClient: React.FC<ReservationsClientProps> = ({
+  reservations,
+  currentUser,
+}) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState("");
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+
+      axios
+        .delete(`/api/reservations/${id}`)
+        .then(() => {
+          toast.success("Reservation cancelled");
+        })
+        .catch(() => {
+          toast.error("Something went wrong");
+        })
+        .finally(() => {
+          setDeletingId("");
+        });
+    },
+    [router]
+  );
+
+  return (
+    <Container>
+      <Heading title="Reservations" subtitle="Bookings on your properties" />
+      <div className="mt-10 gap-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {reservations.map((reservation) => (
+          <ListingCard
+            key={reservation.id}
+            data={reservation.listing}
+            reservation={reservation}
+            actionId={reservation.id}
+            onAction={onCancel}
+            disabled={deletingId === reservation.id}
+            actionLabel="Cancel guest reservation"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default ReservationsClient;
